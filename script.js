f6500d0d1336baa4fd1e0c3bbd0a74994e3be483
satz15 <sathishsekar154@gmail.com>
2023-10-12 10:19:18 +0530

    do

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-`use strict`
+'use strict';
 
-const firstTitle= document.getElementById(`first`)
+const firstTitle = document.getElementById('first');
+const one = document.getElementById('one');
+console.log(firstTitle);
 
-firstTitle.addEventListener(`click`,()=>{
-    
-})
+firstTitle.addEventListener('change', () => {
+    const firstTitleEl = firstTitle.checked; // Use the `checked` property for checkboxes
+    console.log(firstTitleEl);
+
+    if (firstTitleEl) {
+        one.classList.add('strike');
+    } else {
+        one.classList.remove('strike');
+    }
+});
+
+
