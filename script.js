7f6d6f1d5473b783be1ac87c1ba3d0727f68d574
satz15 <sathishsekar154@gmail.com>
2023-12-27 14:07:46 +0530

    updated

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 'use strict';
 
-const firstTitle = document.getElementById('first');
+const firstTitle = document.querySelectorAll('.lines');
 const one = document.getElementById('one');
 console.log(firstTitle);
 
-firstTitle.addEventListener('change', () => {
-    const firstTitleEl = firstTitle.checked; 
-    console.log(firstTitleEl);
+// firstTitle.addEventListener('change', () => {
+//     const firstTitleEl = firstTitle.checked; 
+//     console.log(firstTitleEl);
 
-    if (firstTitleEl) {
-        one.classList.add('strike');
-    } else {
-        one.classList.remove('strike');
-    }
-});
+//     if (firstTitleEl) {
+//         one.classList.add('strike');
+//     } else {
+//         one.classList.remove('strike');
+//     }
+// });
+
+firstTitle.map{{line} => ()
+   line.add
+}
+
+// const secondTitle = document.getElementById('second');
+// const two = document.getElementById('two');
+// console.log(secondTitle);
+
+// firstTitle.addEventListener('change', () => {
+//     const secondTitleEl = secondTitle.checked; 
+//     console.log(secondTitleEl);
+
+//     if (secondTitleEl) {
+//         two.classList.add('strike');
+//     } else {
+//         two.classList.remove('strike');
+//     }
+// });
 
 
